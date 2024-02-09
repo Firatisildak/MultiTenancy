17cfb9b0d24a0eb3f795931c103bd7f25b53c5bc
Fırat Işıldak <firatisildak1@gmail.com>
2024-02-09 10:18:59 +0000

    Services layer added

--- MultiTenancy/GlobalUsings.cs
+++ MultiTenancy/GlobalUsings.cs
@@ -1,2 +1,3 @@
 ﻿global using MultiTenancy.Contract;
 global using MultiTenancy.Settings;
+global using MultiTenancy.Services;
--- MultiTenancy/obj/Debug/net8.0/MultiTenancy.AssemblyInfo.cs
+++ MultiTenancy/obj/Debug/net8.0/MultiTenancy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiTenancy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1f93bdf80cbaac00e09594efa3b06814d5f5e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiTenancy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiTenancy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
